c99f57175eff2136214c45d66fc0ed90b7bd0007
david.vega@snhu.edu <david.vega@snhu.edu>
2018-09-01 23:07:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artist: "songs"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-playlist.artistName = "songTitle";
+playlist[artistName] = "songTitle";
 
 return playlist;
 }
